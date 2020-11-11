a43bcd159054242e023a1c0abe3dedd126ae8c83
Brittany <brittanyabarnard@gmail.com>
2020-11-10 19:54:57 -0500

    initial project structure

---
+++ script.js
@@ -0,0 +1,15 @@
+//user has 4 questions with options a, b, c, d
+    //(form input type=radio to select)
+
+    //user clicks on an option for each question
+    //create something to store answer
+
+//submit form button
+
+//IF option a return "gym rat"
+//ELSE if option b return "regular gym goer"
+//ELSE if option c return ""
+//else option d return
+
+//display results to the page
+
