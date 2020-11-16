ade8af3f02832e2ad0d30cc3657eda768b00d9aa
Brittany <brittanyabarnard@gmail.com>
2020-11-16 02:14:43 -0500

    almost there

--- script.js
+++ script.js
@@ -13,3 +13,46 @@
 
 //display results to the page
 
+
+const gymPersonalityApp = {};
+
+const gymStereotypes = [cardioBunny, bodyBuilder, everydayGymGoer];
+// console.log(gymStereotypes);
+
+gymPersonalityApp.init = () => {
+
+}
+
+$(function(){
+    $("form").on("submit", function(e){
+        e.preventDefault();
+        // console.log("formhasbeensubmitted");
+
+        const usersAnswers = [
+            $("input[name=question1]:checked").val(),
+            $("input[name=question2]:checked").val(),
+            $("input[name=question3]:checked").val(),
+            $("input[name=question4]:checked").val(),
+        ]
+        // console.log(usersAnswers);
+        const totalUsersAnswers = {};
+
+        for (let i = 0; i < usersAnswers.length; i++) {
+            const gymStereotype = usersAnswers[i];
+            if (totalUsersAnswers[gymStereotype]) {
+                totalUsersAnswers[gymStereotype] = totalUsersAnswers[gymStereotype] + 1;
+            } else if (!totalUsersAnswers[gymStereotype]) {
+                totalUsersAnswers[gymStereotype] = 1;
+            }
+            console.log(gymStereotype);
+        }
+    })
+    gymPersonalityApp.init();
+})
+
+
+//document ready:
+$(function () {
+})
+
+
